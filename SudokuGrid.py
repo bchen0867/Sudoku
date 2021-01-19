cab92b3f3d708b509e158fe9f2b80cf678962446
bichen <chenbi0867@gmail.com>
2021-01-19 14:10:50 -0800

    organized keyboard events

--- SudokuGrid.py
+++ SudokuGrid.py
@@ -7,6 +7,7 @@ from datetime import timedelta
 from SudokuCell import SudokuCell
 from utilities.Button import *
 from utilities.colors import *
+from utilities.handle_events import *
 import numpy as np
 
 
@@ -222,29 +223,31 @@ if __name__ == '__main__':
                 if event.key == pg.K_q:
                     run = False
 
-                if pg.K_1 <= event.key <= pg.K_9:
-                    key = int(pg.key.name(event.key))
-                    # update temp info only after event
-                    if board.selected and key is not None:
-                        board.update_selected_cell(key)
+                num_input_events(event, board)
+                # if pg.K_1 <= event.key <= pg.K_9:
+                #     key = int(pg.key.name(event.key))
+                #     # update temp info only after event
+                #     if board.selected and key is not None:
+                #         board.update_selected_cell(key)
+                #
+                # if pg.K_KP1 <= event.key <= pg.K_KP9:
+                #     key = int(pg.key.name(event.key)[1])
+                #     # update temp info only after event
+                #     if board.selected and key is not None:
+                #         board.update_selected_cell(key)
 
-                if pg.K_KP1 <= event.key <= pg.K_KP9:
-                    key = int(pg.key.name(event.key)[1])
-                    # update temp info only after event
-                    if board.selected and key is not None:
-                        board.update_selected_cell(key)
-
-                if board.selected:
-                    i, j = board.selected
-                    if event.key == pg.K_LEFT and j > 0:
-                        j -= 1
-                    elif event.key == pg.K_RIGHT and j < 8:
-                        j += 1
-                    elif event.key == pg.K_UP and i > 0:
-                        i -= 1
-                    elif event.key == pg.K_DOWN and i < 8:
-                        i += 1
-                    board.select(i, j)
+                arrow_keys_events(event, board)
+                # if board.selected:
+                #     i, j = board.selected
+                #     if event.key == pg.K_LEFT and j > 0:
+                #         j -= 1
+                #     elif event.key == pg.K_RIGHT and j < 8:
+                #         j += 1
+                #     elif event.key == pg.K_UP and i > 0:
+                #         i -= 1
+                #     elif event.key == pg.K_DOWN and i < 8:
+                #         i += 1
+                #     board.select(i, j)
 
                 if event.key == pg.K_DELETE:
                     board.clear_selected()
